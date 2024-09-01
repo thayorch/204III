250d7d951c995f0f6787eb5360772fe41159d137
thayorch <thatcharnon.10210@gmail.com>
2024-09-02 01:26:28 +0700

    HW10

--- HW_10/HW10_2_670510662.py
+++ HW_10/HW10_2_670510662.py
@@ -8,43 +8,36 @@
 # กำหนดให้แต่ละบริษัทมีรถในขบวนอย่างน้อยหนึ่งคัน โดย String ของลำดับที่เป็น Output จะอยู่ในรูป เลขประจำรถคั่นด้วย '>'
 
 def arrival_sequences(l_lane: tuple[str], r_lane: tuple[str], result=[]) -> list[str]:
+    l_lane = list(l_lane)
+    r_lane = list(r_lane)
     
     l_len = len(l_lane)
     r_len = len(r_lane)
     
-    if l_len != 0 :
-        l_tail = l_lane[1:]
-    if r_len != 0 :
-        r_tail = r_lane[1:]
-    
     if l_len == 0 and r_len == 0:
         return result
+    
+    if l_len > 0 :
+        # l_head = l_lane[0]
+        result.append(l_lane.pop(0))
+        l_tail = l_lane[:]
+    if r_len > 0 :
+        # r_head = r_lane[0] 
+        result.append(r_lane.pop(0))
+        r_tail = r_lane[:]
 
 # append head
-    if l_len>0 and r_len>0:
-        result.append(l_lane[0]+'>'+r_lane[0])
-        return arrival_sequences(l_tail, r_tail, result)
-    
+    return arrival_sequences(l_tail,r_tail,result)
 
-    
-    
-    # for i in range(l_len-1):
-        # result.append(l_head)
-        # result.append(r_head)
-        # result.append("".join(l_tail[i]))
-        # for j in range(r_len-1):
-            # result.append("".join(r_tail[j]))
-            
-    return result
     
 
 if __name__ == '__main__':
     print("Testing...")
     # print(arrival_sequences(('R32',), ('O9', 'O5')))
     print(arrival_sequences(('R2','R4'), ('O34', 'O22')))
-    assert arrival_sequences(('R32',), ('O9', 'O5')) == ['O9>O5>R32',
-'O9>R32>O5',
-'R32>O9>O5']   
+#     assert arrival_sequences(('R32',), ('O9', 'O5')) == ['O9>O5>R32',
+# 'O9>R32>O5',
+# 'R32>O9>O5']   
     assert arrival_sequences(('R2','R4'), ('O34', 'O22')) == ['R2>R4>O34>O22',
 'R2>O34>R4>O22',
 'R2>O34>O22>R4',
