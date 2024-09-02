7e42650635ce047879363d1865024dab94d8ad43
thayorch <thatcharnon.10210@gmail.com>
2024-09-02 23:00:29 +0700

    EXTAR

--- HW_10/HW10_EX_670510662.py
+++ HW_10/HW10_EX_670510662.py
@@ -25,31 +25,40 @@ __________'''
 
 def main():
     # print(cat_altar(int(input())))
-    print(cat_altar(1))
-    print(cat_altar(2))
-    print(cat_altar(3))
+    # print(cat_altar(1))
+    # print(cat_altar(2))
+    # print(cat_altar(3))
+    # print(cat_altar(4))
+    # print(cat_altar(5))
+    print(cat_altar(6))
 
 
 def cat_altar(n):
+    result = []
+    if n == 1:
+        return cat_head
+    # Top
     for i in range(len(cat_head.split('\n'))):
         cat_head1 = "".join(cat_head.split('\n')[i])
-        cat_head_and_space = " "*(6*n+2)+cat_head1
-        print(cat_head_and_space)
+        cat_head_and_space = (" "*10*(n-1))+cat_head1
+        result.append(cat_head_and_space)
+        
+        
     for j in range(n-1):
         for i in range(len(cat_head.split('\n'))):
-            if j==n-2:
-                cat_up1 = "".join(cat_up.split('\n')[i])
-                cat_down1 = "".join(cat_down.split('\n')[i])
-                cat_up_and_space = (cat_up1)
-                cat_down_and_space = (" "*10*n*(n-2))+cat_down1
-                print(cat_up_and_space+cat_down_and_space)   
-            elif j != n:
-                cat_up1 = "".join(cat_up.split('\n')[i])
-                cat_down1 = "".join(cat_down.split('\n')[i])
-                cat_up_and_space = (" "*5*(n-1))+cat_up1
-                cat_down_and_space = (" "*9*(n-2))+cat_down1
-                print(cat_up_and_space+cat_down_and_space)                   
-    return
+            
+            cat_up1 = "".join(cat_up.split('\n')[i])
+            # if j <= 0:
+            cat_up_and_space = (" "*(10*((n-j)-n))) + cat_up1
+            
+            cat_down1 = "".join(cat_down.split('\n')[i])
+            # cat_up_and_space = (" "*10*((n+j)-n))+cat_up1
+            
+            cat_down_and_space = (" "*(10*((n+j)-n+1))) + cat_down1
+            
+            result.append(cat_up_and_space + cat_down_and_space) 
+            # print(j)
+    return "\n".join(result)
 
 
 if __name__ == '__main__':
