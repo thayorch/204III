1b325a22b9d877ef06a697c5637674169b76cdb0
thayorch <thatcharnon.10210@gmail.com>
2024-09-14 00:03:39 +0700

    HW11_2 Complete

--- HW_11/HW11_2_670510662.py
+++ HW_11/HW11_2_670510662.py
@@ -3,45 +3,59 @@
 # 670510662
 # HW11_1
 # 204111 Sec 001
-   
 
-# def permu(seq1, seq2, res=()):
-#     if not seq1 and not seq2:
-#         return ['>'.join(res + seq1 + seq2)]
-#     p1 = permu(seq1[1:], seq2, res + (seq1[0],))
-#     p2 = permu(seq1, seq2[1:], res + (seq2[0],))
-#     return p1 + p2
+def power_set(s):
+    result = []
+    def subset(index, current):
+        if index == len(s):
+            result.append(current[:])
+            return
+        
+        subset(index + 1, current)
+        
+        current.append(s[index])
+        subset(index + 1, current)
+        
+        current.pop()
+    
+    subset(0, [])
+    return result
+
+
 
 def split_and_merge(n: int) -> list[str]:
     # Slide w11_a_lec_1_recursion_part_ii [Assignment Page 16]
+    full_set = [str(i) for i in range(1, n + 1)] 
+    power_set_list = power_set(full_set)
+    result = set()
     
-    base_object = tuple(map(str,range(1,n+1)))  
-    result = []
-
-    for i in range(len(base_object)):
-        new_list = tuple(map(str, filter(lambda x: x != base_object[i], base_object)))      
-        result += arrival_sequences((base_object[i],), new_list)
-
-    return sorted(list(set(result)))
-
-
-def arrival_sequences(l_lane: tuple[str], r_lane: tuple[str]) -> list[str]:
-    list_of_seq = []
-    def generate_seq(l_lane, r_lane, result: list, list_of_seq: list, i=0, j=0):
-        if i == len(l_lane) and j == len(r_lane):
-            list_of_seq.append('>'.join(result))
-            return None
-        if i < len(l_lane):
-            generate_seq(l_lane, r_lane, result + [l_lane[i]], list_of_seq,  i + 1, j)
-        if j < len(r_lane):
-            generate_seq(l_lane, r_lane, result + [r_lane[j]], list_of_seq, i, j + 1)    
+    for l_lane in power_set_list:
+        r_lane = [x for x in full_set if x not in l_lane]
+        result |= (arrival_sequences(l_lane, r_lane))
     
-    generate_seq(l_lane, r_lane, [], list_of_seq)
-    return list_of_seq
-    
+    return list(result)
+
+
+
+def arrival_sequences(l_lane: tuple[str], r_lane: tuple[str]) -> set[str]:
+    def arr_rec(l_lane, r_lane):
+        #base case
+        if not l_lane:
+            return {'>'.join(r_lane)}
+        if not r_lane:
+            return {'>'.join(l_lane)}
+
+        temp = arr_rec(l_lane[1:], r_lane)
+        q1 = set(map(lambda x: l_lane[0]+'>'+x, temp))
+        
+        temp = arr_rec(l_lane, r_lane[1:])
+        q2 = set(map(lambda x: r_lane[0]+'>'+x, temp))
+
+        return q1 | q2
+    return arr_rec(l_lane, r_lane)
 
 if __name__ == '__main__':
-    # print(split_and_merge(3))   # ['1>2>3', '1>3>2', '2>1>3', '2>3>1','3>1>2']
-    print(split_and_merge(4))   # ['2>2>3>4', '1>2>4>3', '1>3>2>4', '1>3>4>2', '1>4>2>3',
-                                # '2>1>3>4','2>1>4>3', '2>3>1>4', '2>3>4>1',
+    print(split_and_merge(3))   # ['1>2>3', '1>3>2', '2>1>3', '2>3>1','3>1>2']
+    # print(split_and_merge(4))   # ['2>2>3>4', '1>2>4>3', '1>3>2>4', '1>3>4>2', '1>4>2>3',
+    #                             # '2>1>3>4','2>1>4>3', '2>3>1>4', '2>3>4>1',
                                 # '2>4>1>3', '3>1>2>4', '3>1>4>2', '3>4>1>2', '4>1>2>3']
